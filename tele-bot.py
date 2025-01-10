1229432b816e69e543e712dc1bfc64a10cf6195b
Minh Pham <67337723+minhp03@users.noreply.github.com>
2025-01-09 21:35:13 -0500

    Update tele-bot.py
    update code for the bot , running successfully through telegram

--- tele-bot.py
+++ tele-bot.py
@@ -1,12 +1,5 @@
 import os
 from dotenv import load_dotenv
-
-from telegram import Update
-from telegram.ext import ApplicationBuilder,ContextTypes, CommandHandler
-
-
-import os
-from dotenv import load_dotenv
 from openai import OpenAI
 from telegram import Update
 from telegram.ext import filters,ApplicationBuilder,ContextTypes, CommandHandler, MessageHandler
@@ -40,5 +33,29 @@ async def chat(update:Update, context: ContextTypes.DEFAULT_TYPE):
         model="gpt-4o-mini",
         #stream=True
     )
+    #update chat history
+    bot_message = chat_completion.choices[0].message.content
+    chat_history.append([user_message,bot_message])
 
+    #update bot message
+    # each bot have a different chat_id, so we update effective_chat id to send message 
+    await context.bot.send_message(chat_id=update.effective_chat.id, 
+                                   text=bot_message)
+    
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Hàm này sẽ được gọi khi bạn gửi lệnh /start
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Mình là bot, bạn có câu hỏi gì không!")
+
+
+
+#
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+start_handler = CommandHandler("start", start)
+application.add_handler(start_handler)
+
+chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
+application.add_handler(chat_handler)
+
+print("Bot is running")
 application.run_polling()
